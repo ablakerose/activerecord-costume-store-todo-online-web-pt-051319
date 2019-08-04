40384288cd2f36954f93069f822cbff84cf67574
Blake Rose <bdecktor@gmail.com>
2019-08-04 22:10:46 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :location
+      t.string :costume_inventory
+      t.integer :employee_count
+      t.boolean :open_closed
+      t.timestamps
+    end
+  end
+end
